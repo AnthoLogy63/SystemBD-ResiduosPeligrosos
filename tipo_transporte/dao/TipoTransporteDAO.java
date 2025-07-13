ea884931b1f9885976bc5f62a38c5ecc0c427ebe
FernandoGarambelM <fgarambel@unsa.edu.pe>
2025-07-12 21:16:29 -0500

    Se modifico el DAO de tipo transporte para la conexion con MySQL

--- tipo_transporte/dao/TipoTransporteDAO.java
+++ tipo_transporte/dao/TipoTransporteDAO.java
@@ -1,23 +1,19 @@
 package tipo_transporte.dao;
 
 import tipo_transporte.modelo.TipoTransporteModel;
-
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
-
-import src.DBConnection;
+import src.DBConnectionMySQL; // Asegúrate de usar esta clase
 
 public class TipoTransporteDAO {
 
     public List<TipoTransporteModel> findAll() throws SQLException {
         List<TipoTransporteModel> lista = new ArrayList<>();
-        String sql = "SELECT \"TipTransCod\", \"TipTransNom\", \"TipTransEst\" "
-                + "FROM public.\"GZZ_TIPOTRANSPORTE\" "
-                + "ORDER BY \"TipTransCod\" ASC";
-        try (Connection conn = DBConnection.getConnection();
-            PreparedStatement ps = conn.prepareStatement(sql);
-            ResultSet rs = ps.executeQuery()) {
+        String sql = "SELECT TipTransCod, TipTransNom, TipTransEst FROM GZZ_TIPOTRANSPORTE ORDER BY TipTransCod ASC";
+        try (Connection conn = DBConnectionMySQL.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql);
+             ResultSet rs = ps.executeQuery()) {
             while (rs.next()) {
                 lista.add(new TipoTransporteModel(
                     rs.getString("TipTransCod"),
@@ -29,10 +25,9 @@ public class TipoTransporteDAO {
         return lista;
     }
 
-
     public void insert(TipoTransporteModel tt) throws SQLException {
-        String sql = "INSERT INTO public.\"GZZ_TIPOTRANSPORTE\" (\"TipTransCod\",\"TipTransNom\",\"TipTransEst\") VALUES (?,?,?)";
-        try (Connection conn = DBConnection.getConnection();
+        String sql = "INSERT INTO GZZ_TIPOTRANSPORTE (TipTransCod, TipTransNom, TipTransEst) VALUES (?, ?, ?)";
+        try (Connection conn = DBConnectionMySQL.getConnection();
              PreparedStatement ps = conn.prepareStatement(sql)) {
             ps.setString(1, tt.getCodigo());
             ps.setString(2, tt.getNombre());
@@ -42,8 +37,8 @@ public class TipoTransporteDAO {
     }
 
     public void update(TipoTransporteModel tt) throws SQLException {
-        String sql = "UPDATE public.\"GZZ_TIPOTRANSPORTE\" SET \"TipTransNom\" = ?, \"TipTransEst\" = ? WHERE \"TipTransCod\" = ?";
-        try (Connection conn = DBConnection.getConnection();
+        String sql = "UPDATE GZZ_TIPOTRANSPORTE SET TipTransNom = ?, TipTransEst = ? WHERE TipTransCod = ?";
+        try (Connection conn = DBConnectionMySQL.getConnection();
              PreparedStatement ps = conn.prepareStatement(sql)) {
             ps.setString(1, tt.getNombre());
             ps.setString(2, tt.getEstado());
@@ -52,13 +47,12 @@ public class TipoTransporteDAO {
         }
     }
 
-    //Marcar como “eliminado”
     public boolean softDelete(String codigo) throws SQLException {
-        String select = "SELECT \"TipTransEst\" FROM public.\"GZZ_TIPOTRANSPORTE\" WHERE \"TipTransCod\" = ?";
-        String update = "UPDATE public.\"GZZ_TIPOTRANSPORTE\" SET \"TipTransEst\" = '*' WHERE \"TipTransCod\" = ?";
+        String select = "SELECT TipTransEst FROM GZZ_TIPOTRANSPORTE WHERE TipTransCod = ?";
+        String update = "UPDATE GZZ_TIPOTRANSPORTE SET TipTransEst = '*' WHERE TipTransCod = ?";
 
-        try (Connection conn = DBConnection.getConnection();
-            PreparedStatement ps1 = conn.prepareStatement(select)) {
+        try (Connection conn = DBConnectionMySQL.getConnection();
+             PreparedStatement ps1 = conn.prepareStatement(select)) {
             ps1.setString(1, codigo);
             try (ResultSet rs = ps1.executeQuery()) {
                 if (rs.next() && "*".equals(rs.getString(1))) {
@@ -74,17 +68,16 @@ public class TipoTransporteDAO {
         }
     }
 
-    //Inactivar: estado 'I'
     public boolean inactivate(String codigo) throws SQLException {
-        String select = "SELECT \"TipTransEst\" FROM public.\"GZZ_TIPOTRANSPORTE\" WHERE \"TipTransCod\" = ?";
-        String update = "UPDATE public.\"GZZ_TIPOTRANSPORTE\" SET \"TipTransEst\" = 'I' WHERE \"TipTransCod\" = ?";
+        String select = "SELECT TipTransEst FROM GZZ_TIPOTRANSPORTE WHERE TipTransCod = ?";
+        String update = "UPDATE GZZ_TIPOTRANSPORTE SET TipTransEst = 'I' WHERE TipTransCod = ?";
 
-        try (Connection conn = DBConnection.getConnection();
-            PreparedStatement ps1 = conn.prepareStatement(select)) {
+        try (Connection conn = DBConnectionMySQL.getConnection();
+             PreparedStatement ps1 = conn.prepareStatement(select)) {
             ps1.setString(1, codigo);
             try (ResultSet rs = ps1.executeQuery()) {
                 if (rs.next() && "I".equals(rs.getString(1))) {
-                    return false; 
+                    return false;
                 }
             }
 
@@ -96,18 +89,16 @@ public class TipoTransporteDAO {
         }
     }
 
-
-    //Reactivar: estado 'A'
     public boolean reactivate(String codigo) throws SQLException {
-        String select = "SELECT \"TipTransEst\" FROM public.\"GZZ_TIPOTRANSPORTE\" WHERE \"TipTransCod\" = ?";
-        String update = "UPDATE public.\"GZZ_TIPOTRANSPORTE\" SET \"TipTransEst\" = 'A' WHERE \"TipTransCod\" = ?";
+        String select = "SELECT TipTransEst FROM GZZ_TIPOTRANSPORTE WHERE TipTransCod = ?";
+        String update = "UPDATE GZZ_TIPOTRANSPORTE SET TipTransEst = 'A' WHERE TipTransCod = ?";
 
-        try (Connection conn = DBConnection.getConnection();
-            PreparedStatement ps1 = conn.prepareStatement(select)) {
+        try (Connection conn = DBConnectionMySQL.getConnection();
+             PreparedStatement ps1 = conn.prepareStatement(select)) {
             ps1.setString(1, codigo);
             try (ResultSet rs = ps1.executeQuery()) {
                 if (rs.next() && "A".equals(rs.getString(1))) {
-                    return false; 
+                    return false;
                 }
             }
 
@@ -120,9 +111,9 @@ public class TipoTransporteDAO {
     }
 
     public TipoTransporteModel findById(String codigo) throws SQLException {
-        String sql = "SELECT \"TipTransCod\",\"TipTransNom\",\"TipTransEst\" FROM public.\"GZZ_TIPOTRANSPORTE\" WHERE \"TipTransCod\" = ?";
-        try (Connection conn = DBConnection.getConnection();
-            PreparedStatement ps = conn.prepareStatement(sql)) {
+        String sql = "SELECT TipTransCod, TipTransNom, TipTransEst FROM GZZ_TIPOTRANSPORTE WHERE TipTransCod = ?";
+        try (Connection conn = DBConnectionMySQL.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
             ps.setString(1, codigo);
             try (ResultSet rs = ps.executeQuery()) {
                 if (rs.next()) {
